96527bf82b254c966cc10759e0894d6a91c58c3b
Giuseppe Toto <giuseppe.toto@gmail.com>
2016-07-02 18:37:55 +0200

    add info product feed

--- src/Models/ProductFeed.php
+++ src/Models/ProductFeed.php
@@ -21,5 +21,31 @@ class ProductFeed extends Product
 
     public $variantsGroup;
 
+    public $descriptions;
+
+    public function getSize()
+    {
+
+        if (isset($this->attributeGroups['attributeGroupList']) && count($this->attributeGroups['attributeGroupList'])>0) {
+            foreach ($this->attributeGroups['attributeGroupList'][0]['attributeList'] as $attr) {
+                if ($attr['attributeCode'] == 'size') {
+                    return $attr['localeName'];
+                }
+            }
+            return null;
+        }
+    }
+
+    public function getColor()
+    {
+        if (isset($this->attributeGroups['attributeGroupList'])&& count($this->attributeGroups['attributeGroupList'])>0) {
+            foreach ($this->attributeGroups['attributeGroupList'][0]['attributeList'] as $attr) {
+                if ($attr['attributeCode'] == 'color') {
+                    return $attr['localeName'];
+                }
+            }
+            return null;
+        }
+    }
 
 }
