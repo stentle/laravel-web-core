f9935d3951f6601f884935c960abe223c60aba09
Fabio Severino <fabio.severino32@monetas.net>
2016-09-16 19:00:37 +0200

    cart model improved a bit

--- src/Models/Cart.php
+++ src/Models/Cart.php
@@ -1,11 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: giuseppetoto
- * Date: 10/07/15
- * Time: 10:56
- */
-
 namespace Stentle\LaravelWebcore\Models;
 
 use Illuminate\Support\Facades\Session;
@@ -15,8 +8,6 @@ use Stentle\LaravelWebcore\Http\RestModel;
 
 class Cart extends RestModel
 {
-
-
     protected $resource = 'carts';
     public $productCartList;
     public $itemQuantity;
@@ -26,18 +17,14 @@ class Cart extends RestModel
     public $totals;
     public $country;
     public $message;
-
     public $settings;
-
-
 
     public static function getCartFromSession()
     {
         $country_active = Localization::getCountryRegionActive();
         $carts = Session::get('carts');
-        if (is_array($carts) && isset($carts[$country_active])) {
+        if (is_array($carts) && isset($carts[$country_active]))
             return $carts[$country_active];
-        }
 
         return null;
     }
@@ -46,8 +33,10 @@ class Cart extends RestModel
     {
         $country_active = Localization::getCountryRegionActive();
         $carts = Session::get('carts');
+
         if (!is_array($carts))
             $carts = [];
+
         $carts[$country_active] = $cart->getInfo();
         Session::put('carts', $carts);
         Cart::activeCartInSession($cart);
@@ -56,67 +45,70 @@ class Cart extends RestModel
     private static function activeCartInSession($cart)
     {
         setcookie('cart_id', $cart->id, time() + env('SESSION_DURATION') * 60, '/');
-        $_COOKIE['cart_id'] = $cart->id;
     }
 
-    /**Si occupa di creare un carrello con un prodotto specifico e dei settings
+    /**
+     * Si occupa di creare un carrello con un prodotto specifico e dei settings
      * @param $product_id
      * @param array $settings
      * @param int $quantity
      * @return bool|Cart
      */
-
     public static function create($product_id, $settings = [], $quantity = 1)
     {
-
         $cart = new Cart();
 
         $cart->productCartList = array();
         $cart->productCartList[] = array('id' => $product_id, 'requestedQuantity' => $quantity);
+
         if (count($settings) > 0)
             $cart->settings = $settings;
-        if ($cart->save() && !empty($cart->id)) {
+
+        if ($cart->save() && !empty($cart->id))
+        {
             Cart::storeCartInSession($cart);
             return $cart;
-        } else {
-            return false;
         }
+        else
+            return false;
     }
 
     public function checkout($settings)
     {
-
-        if ($this->id != null) {
+        if ($this->id != null)
+        {
             $options['json'] = $settings;
 
             $response = ClientHttp::post($this->resource . '/' . $this->id . '/checkout', $options);
 
-            if ($response->getStatusCode() == 200 || $response->getStatusCode() == 201) {
+            if ($response->getStatusCode() == 200 || $response->getStatusCode() == 201)
+            {
                 $json = json_decode($response->getBody()->getContents(), true);
                 if (isset($json['data']))
                     $this->setInfo($json['data']);
                 return $json;
-            } else {
-                return false;
             }
-        } else
+            else
+                return false;
+        }
+        else
             return false;
-
     }
 
     /**
      * Esegue il checkout con paypal.
-     * Paypal offre la possibilità di eseguire il pagamento diretto o con prelievo automatico in un momento successivo($preapproval=true).
-     * Nell'ultimo caso  se l'utente non ha mai fornito l'autorizzazione ai prelievi automatici l'api fornisce un intent per fare la redirect a paypal,
-     * altrimenti fornisce direttamente il carrello creato.
+     * Paypal offre la possibilità di eseguire il pagamento diretto o con prelievo automatico in un momento successivo
+     * ($preapproval=true). Nell'ultimo caso  se l'utente non ha mai fornito l'autorizzazione ai prelievi automatici
+     * l'api fornisce un intent per fare la redirect a paypal, altrimenti fornisce direttamente il carrello creato.
+     *
      * @param $url_success  La url che paypal direzionerà l'utente dopo il corretto pagamento
      * @param $url_failure   La url che paypal direzionerà l'utente in caso il pagamento fallisce
      * @param $preapproval se true, all'utente verrà richiesta un'autorizzazione ai prelievi automatici. Se false, si procede al pagamento diretto.
      * @param string $mode paypal ha due modalità express checkout e adaptive. Stentle attualmente utilizza quest'ultima modalità.
+     * @return bool|mixed
      */
     public function checkoutWithPaypal($url_success, $url_failure, $preapproval, $mode = 'adaptive')
     {
-
         $config['paymentService'] = 'paypal';
         $config['paypalMode'] = $mode;
         $config['paypalCancelUrl'] = $url_failure;
@@ -125,40 +117,45 @@ class Cart extends RestModel
         return $this->checkout($config);
     }
 
-    /** Questa chiamata va eseguita una volta che è stato effettuato il pagamento con paypal e per forzare l'api a fare il checkout finale.
-     * @param $preapproval
+    /**
+     * Questa chiamata va eseguita una volta che è stato effettuato il pagamento con paypal e per forzare l'api a fare
+     * il checkout finale.
+     * @param $preApproval
      * @param string $mode
      * @return bool|mixed
      */
-    public function completeCheckoutWithPaypal($preapproval='true', $mode = 'adaptive'){
-        if($this->status=='CART_PAYING') {
+    public function completeCheckoutWithPaypal($preApproval = 'true', $mode = 'adaptive')
+    {
+        if ($this->status == 'CART_PAYING')
+        {
             $config['paymentService'] = 'paypal';
             $config['paypalMode'] = $mode;
             $config['paypalCancelUrl'] = '';
             $config['paypalReturnUrl'] = '';
-            $config['paypalPreapproval'] = $preapproval;
+            $config['paypalPreapproval'] = $preApproval;
             return $this->checkout($config);
-        }else{
-            return false;
         }
+        else
+            return false;
     }
 
     public function setShippingAddress()
     {
-
-        if ($this->id != null) {
-
-        } else {
-            throw new \Exception("cart_id is notdefined");
+        if ($this->id != null)
+        {
+            //TODO: handle this (1)
         }
+        else
+            throw new \Exception("cart_id is not defined");
     }
 
     public function setBillingAddress()
     {
-        if ($this->id != null) {
-
-        } else {
-            throw new \Exception("cart_id is notdefined");
+        if ($this->id != null)
+        {
+            //TODO: handle this (2)
         }
+        else
+            throw new \Exception("cart_id is not defined");
     }
 }
