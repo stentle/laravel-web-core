4c474dbb70959e839c870dd68dac69887cef8d20
Lorenzo Ligato <lorenzo.ligato@gmail.com>
2018-10-30 10:13:40 +0100

    To show Micro and Marco into Product Model.

--- src/Models/ProductFeed.php
+++ src/Models/ProductFeed.php
@@ -34,6 +34,8 @@ class ProductFeed extends Product
     public $sellingPrice;
     public $currency;
     public $coverPhotoUrl;
+    public $microProductCategory;
+    public $macroProductCategory;
 
 
     public function getSize()
@@ -335,6 +337,7 @@ class ProductFeed extends Product
 
         $products = [];
         $items = $json['data']['result']['items'];
+        
 
         foreach ($json['data']['result']['items'] as $item) {
 
@@ -342,6 +345,8 @@ class ProductFeed extends Product
             $p->setInfo($item);
             $products[] = $p;
         }
+
+        //dd($products);
 
         $json['data']['result']['items'] = $products;
         return $json;
